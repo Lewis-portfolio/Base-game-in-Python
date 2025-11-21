735349898c9d8a9b6c71fec665bbd205e03b2da9
Lewis-portfolio <132489456+Lewis-portfolio@users.noreply.github.com>
2025-11-21 13:32:06 +0000

    Created a base object class

---
+++ Python_Files/Classes/base_sprite_class.py
@@ -0,0 +1,18 @@
+# My base game - Base sprite class
+# Start date: 05, 11, 2025
+# By: Lewis Burt
+''' This is the Base Class for my game in python. '''
+
+# Imports:
+import pygame as PG
+
+
+class BaseObjectClass(PG.sprite.Sprite):
+    """ The basics of every object in the game. """
+    def __init__(self, game, groups, pos, w, h):
+        self.groups = groups
+        PG.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.rect = PG.Rect((pos), w, h)
+        self.pos = pos
+        self.rect.x , self.rect.y = pos
